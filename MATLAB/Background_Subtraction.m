45330590011727c4a3bd6ba08391ba004564763c
symB10S <jonathandotmeerholz@gmail.com>
2020-09-10 15:54:32 +0200

    X Y and Z of inverse matrix

--- MATLAB/Background_Subtraction.m
+++ MATLAB/Background_Subtraction.m
@@ -1,13 +1,18 @@
-Vid = VideoReader("Sample_Video/test_only_orange.mov");
-scale_factor = 0.1;
-background_threshold = 80;
+video_path = "OneVehicle/Rendered Animation/onevehiclerender.mkv";
 
-vid_writer = VideoWriter('Output/test.mp4','MPEG-4');
+Vid = VideoReader(video_path);
+scale_factor = 0.1;
+background_threshold =  10;
+
+vid_writer = VideoWriter('Output/test');
 open(vid_writer);
 
-background = readFrame(Vid);
+%background = readFrame(Vid);
+background = imread("OneVehicle/Background Image/0235.png");
 half_back = imresize(background,scale_factor);
 half_back_gray = double(rgb2gray(half_back));
+
+se = strel('disk',10);
 
 % Step through Video Frames
 counter = 1;
@@ -19,6 +24,8 @@ while hasFrame(Vid)
     Diff = abs(double(rgb2gray(half))-half_back_gray);
     Diff(Diff < background_threshold) = 0;
     Diff(Diff >= background_threshold) = 1;
+    
+    %Diff = imclose(Diff,se);
     
     writeVideo(vid_writer,Diff);
     counter = counter + 1;
